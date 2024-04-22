d3a4404e9d4a8a602f7f8b46649441adb96a6f29
Soeuk_Sophanit <98696662+soeuksophanit@users.noreply.github.com>
2024-04-22 11:05:54 +0700

    setting up project

---
+++ src/app/todo-board/page.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const page = () => {
+  return <div>page todo board</div>;
+};
+
+export default page;
---
+++ src/app/todo-list/page.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const page = () => {
+  return <div>page todo list</div>;
+};
+
+export default page;
